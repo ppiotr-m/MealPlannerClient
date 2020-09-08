cb3161dd58ba4becfea20f6b09c08c207a778158
Piotr Michałkiewicz <piotrek.michalkiewicz@gmail.com>
2020-09-08 05:46:11 +0200

    Recipe comment section implemented

--- app/src/main/java/piotr/michalkiewicz/mealplannerclient/view/recipes/RecipeActivity.java
+++ app/src/main/java/piotr/michalkiewicz/mealplannerclient/view/recipes/RecipeActivity.java
@@ -3,6 +3,8 @@ package piotr.michalkiewicz.mealplannerclient.view.recipes;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.Button;
 import android.widget.HorizontalScrollView;
 import android.widget.ImageView;
@@ -13,8 +15,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import piotr.michalkiewicz.mealplannerclient.R;
+import piotr.michalkiewicz.mealplannerclient.recipes.model.Comment;
 import piotr.michalkiewicz.mealplannerclient.recipes.model.MealTimeRecipe;
 import piotr.michalkiewicz.mealplannerclient.view.recipes.adapters.RecipeImgFragmentAdapter;
 import piotr.michalkiewicz.mealplannerclient.view.recipes.presenters.RecipePresenter;
@@ -28,7 +32,7 @@ import static piotr.michalkiewicz.mealplannerclient.utils.ConstantValues.TAG;
 public class RecipeActivity extends AppCompatActivity implements InitializableViewWithCategory<MealTimeRecipe> {
 
     private HorizontalScrollView horizontalScrollView;
-    private LinearLayout imageContainer;
+    private LinearLayout commentsSectionLinearLayout;
     private RecipeImgFragmentAdapter adapter;
     private ViewPager viewPager;
     private ImageView temp;
@@ -63,7 +67,7 @@ public class RecipeActivity extends AppCompatActivity implements InitializableVi
 
     private void assignUIElements(){
         horizontalScrollView = findViewById(R.id.horizontalScrollView);
-        imageContainer = findViewById(R.id.imageContainer);
+        commentsSectionLinearLayout = findViewById(R.id.commentsSectionLinearLayout);
         viewPager = findViewById(R.id.viewPager);
         goToCookingStepsBtn = findViewById(R.id.goToCookingStepsBtn);
         goToIngredientsBtn = findViewById(R.id.goToIngredientsBtn);
@@ -109,5 +113,23 @@ public class RecipeActivity extends AppCompatActivity implements InitializableVi
         likedByTV.setText(String.valueOf(data.getTotalLikes()));
         //  TODO Apply proper data
         goodForTV.setText(String.valueOf(data.getSuitableForDiet().get(0)));
+        initCommentSection(Comment.createMockCommentList());
+    }
+
+    private void initCommentSection(List<Comment> commentList) {
+        if (commentList == null || commentList.isEmpty()) return;
+
+        LayoutInflater layoutInflater = LayoutInflater.from(this);
+        for (Comment comment : commentList) {
+            View commentView = layoutInflater.inflate(R.layout.comment_item_layout, null, false);
+
+            TextView usernameTV = commentView.findViewById(R.id.commentUsernameTV);
+            TextView commentTV = commentView.findViewById(R.id.commentTV);
+
+            usernameTV.setText(comment.getUsername());
+            commentTV.setText(comment.getComment());
+
+            commentsSectionLinearLayout.addView(commentView);
+        }
     }
 }
