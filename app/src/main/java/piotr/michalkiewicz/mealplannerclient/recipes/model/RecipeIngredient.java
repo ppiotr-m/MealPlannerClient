4e2b2dc54cf44ad1a1dc966c113aa7e1346ee84b
Piotr Michałkiewicz <piotrek.michalkiewicz@gmail.com>
2020-09-08 05:08:09 +0200

    Ingredient list finished

--- app/src/main/java/piotr/michalkiewicz/mealplannerclient/recipes/model/RecipeIngredient.java
+++ app/src/main/java/piotr/michalkiewicz/mealplannerclient/recipes/model/RecipeIngredient.java
@@ -1,6 +1,7 @@
 package piotr.michalkiewicz.mealplannerclient.recipes.model;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,4 +15,17 @@ public class RecipeIngredient implements Serializable {
     private String originalName;
     private String kind;
     private String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RecipeIngredient that = (RecipeIngredient) o;
+        return originalName.equals(that.originalName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(originalName);
+    }
 }
