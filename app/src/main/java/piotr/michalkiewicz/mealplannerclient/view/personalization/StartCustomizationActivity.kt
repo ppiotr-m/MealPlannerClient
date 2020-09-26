fa91e8bfce9ecbd218e74116e31a6451cfa04f50
Bartosz Libner <bartosz.libner@gmail.com>
2020-09-26 08:06:46 +0000

    Revert "[FOOD-85] Refactored StartCustomizationActivity.java to kt and implemented binding"
    This reverts commit 9ec70136ff319310578c9351f883a1fe7acc84e5

--- app/src/main/java/piotr/michalkiewicz/mealplannerclient/view/personalization/StartCustomizationActivity.kt
+++ app/src/main/java/piotr/michalkiewicz/mealplannerclient/view/personalization/StartCustomizationActivity.kt
@@ -1,30 +1,41 @@
-package piotr.michalkiewicz.mealplannerclient.view.personalization
+package piotr.michalkiewicz.mealplannerclient.view.personalization;
 
-import android.content.Intent
-import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import androidx.appcompat.app.AppCompatActivity
-import piotr.michalkiewicz.mealplannerclient.R
-import piotr.michalkiewicz.mealplannerclient.databinding.ActivityLoginBinding
-import piotr.michalkiewicz.mealplannerclient.databinding.ActivityStartCustomizationBinding
-import piotr.michalkiewicz.mealplannerclient.view.menu.MainMenuActivity
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-class StartCustomizationActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityStartCustomizationBinding
+import androidx.appcompat.app.AppCompatActivity;
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivityStartCustomizationBinding.inflate(layoutInflater)
+import piotr.michalkiewicz.mealplannerclient.R;
+import piotr.michalkiewicz.mealplannerclient.view.menu.MainMenuActivity;
 
-        binding.startCustomizationContent.startCustomizationBtn.setOnClickListener(View.OnClickListener {
-            // TODO Start customization here
-        })
-        binding.startCustomizationContent.skipCustomizationBtn.setOnClickListener(View.OnClickListener {
-            val myIntent = Intent(this@StartCustomizationActivity, MainMenuActivity::class.java)
-            startActivity(myIntent)
-        })
+public class StartCustomizationActivity extends AppCompatActivity {
 
-        setContentView(binding.root)
+    private Button startCustomizationBtn;
+    private Button skipCustomizationBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_customization);
+
+        startCustomizationBtn = findViewById(R.id.startCustomizationBtn);
+        skipCustomizationBtn = findViewById(R.id.skipCustomizationBtn);
+
+        startCustomizationBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO Start customization here
+            }
+        });
+
+        skipCustomizationBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(StartCustomizationActivity.this, MainMenuActivity.class);
+                startActivity(myIntent);
+            }
+        });
     }
 }
